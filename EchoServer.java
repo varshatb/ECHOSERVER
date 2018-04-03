0fe0b8f1e263b01bd11520defccd6c35855ec824
Varsha <varshabhaskar96@gmail.com>
2018-04-03 12:54:43 +0530

    updation7

--- EchoServer.java
+++ EchoServer.java
@@ -20,3 +20,6 @@ public void serve()
     while (true)
     {
     Socket client = server.accept();
+    BufferedReader r = new BufferedReader(new InputStreamReader(client.getInputStream()));
+    PrintWriter w = new PrintWriter(client.getOutputStream(), true);
+    w.println("Welcome to the Java EchoServer.  Type 'bye' to close.");
